f57a61440c68bcfb41841b6777ea3341d57e9859
alitariq2072008-netizen <alitariq2072008@gmail.com>
2026-02-12 18:22:47 +0300

    Add files via upload

---
+++ in.js
@@ -0,0 +1,11 @@
+let heading = document.querySelector('h1');
+heading.textContent = '2';
+let paragraph = document.querySelector('#p1');
+paragraph.textContent = 'plus button';
+let paragraph1 = document.querySelector('#p2');
+paragraph1.textContent = 'minus button';
+
+
+
+
+
